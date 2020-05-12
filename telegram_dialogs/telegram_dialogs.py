9a40dffc7efd4dfe0a6e3cf2b41593f7d29fb250
LayNath242 <laynath242@gmail.com>
2020-05-12 09:03:38 +0700

    fix get dialog

--- telegram_dialogs/telegram_dialogs.py
+++ telegram_dialogs/telegram_dialogs.py
@@ -6,7 +6,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 
 from utils.get_env import api_hash, api_id
-from utils.get_message import get_messages
+from utils.get_message import get_messages, get_lastest_message
 from utils.dowload_file import download_file, download_profile_photo
 from utils.get_entity import get_entity
 from utils._file import create_new_dir
@@ -16,43 +16,25 @@ async def get_all_dialogs(
         auth_key,
         limit
 ):
-    client = TelegramClient(StringSession(auth_key), api_id, api_hash)
-    await client.connect()
+    try:
+        client = TelegramClient(StringSession(auth_key), api_id, api_hash)
+        await client.connect()
+    except:
+        return "client error"
 
     data = []
 
     async for dialog in client.iter_dialogs(limit=limit):
-
         entity = await get_entity(dialog.entity.id, client)
-        # dirname = f"Chat/{dialog.entity.id}/"
-        # create_new_dir(dirname)
-        # filename = f"{dirname}profile/{dialog.name}"
-        # file = await client.download_profile_photo(entity, file=filename, download_big=False)
         filename = await download_profile_photo(entity, client, dialog.entity.id, dialog.name)
+
         user = dialog.message
-        if user.media:
-            try:
-                if user.media.photo:
-                    message = "photo"
-                else:
-                    message = "unknow"
-            except:
-                try:
-                    _type = str(user.media.document.mime_type)
-                    if _type == "image/webp":
-                        message = "sticker"
-                    elif _type == "audio/ogg":
-                        message = "voice message"
-                    elif _type.split("/")[0] == "audio":
-                        message = "audio"
-                    elif _type.split("/")[0] == "video":
-                        message = "video"
-                    else:
-                        message = "media"
-                except:
-                    message = "unknow"
-        else:
-            message = user.message
+
+        try:
+            message = await get_lastest_message(dialog.message, client, entity)
+        except:
+            message = "can not get message"
+
         user = {
             "id": str(dialog.entity.id),
             "name": dialog.name,
@@ -74,31 +56,31 @@ async def get_all_messages(
     ids,
     from_user
 ):
+    try:
+        client = TelegramClient(StringSession(auth_key), api_id, api_hash)
+        await client.connect()
+        messages = []
+        entity = await get_entity(chat_id, client)
 
-    client = TelegramClient(StringSession(auth_key), api_id, api_hash)
-    await client.connect()
+        async for message in client.iter_messages(
+                entity,
+                limit=limit,
+                search=search,
+                reverse=reverse,
+                offset_date=offset_date,
+                ids=ids,
+                from_user=from_user
+        ):
+            filename = None
 
-    messages = []
+            if message.media:
+                filename = await download_file(message.file, chat_id, client)
 
-    entity = await get_entity(chat_id, client)
-
-    async for message in client.iter_messages(
-            entity,
-            limit=limit,
-            search=search,
-            reverse=reverse,
-            offset_date=offset_date,
-            ids=ids,
-            from_user=from_user
-    ):
-        filename = None
-
-        if message.media:
-            filename = await download_file(message.file, chat_id, client)
-
-        user = await client.get_entity(message.from_id)
-        message = get_messages(message, user.username, media=filename)
-        messages.append(message)
+            user = await client.get_entity(message.from_id)
+            message = get_messages(message, user.username, media=filename)
+            messages.append(message)
+    except:
+        message = "error can not get Message"
 
     return messages
 
--- utils/get_message.py
+++ utils/get_message.py
@@ -12,4 +12,52 @@ def get_messages(message, username, media):
     datas.append(data)
     return data
 
-# get lastest_message()
+
+async def get_lastest_message(user, client, entity):
+    if user.geo:
+        message = str(user.geo)
+
+    elif user.venue:
+        message = str(user.venue)
+
+    elif user.invoice:
+        message = str(user.invoice)
+
+    elif user.poll:
+        message = str(user.poll)
+
+    elif user.web_preview:
+        message = user.message
+
+    elif user.contact:
+        message = "contact"
+
+    elif user.game:
+        message = "game"
+
+    elif user.sticker:
+        alt = (user.sticker.attributes)[1].alt
+        message = f"{alt} sticker"
+
+    elif user.gif:
+        message = "GIF"
+
+    elif user.photo:
+        message = "photo"
+
+    elif user.video_note or user.video:
+        message = "video"
+
+    elif user.voice:
+        message = "voice message"
+
+    elif user.is_reply:
+        msg_reply = await client.get_messages(entity, ids=user.reply_to_msg_id)
+        message = {"reply_msg_to": msg_reply.message, "msg": user.text}
+
+    elif user.text or user.raw_text:
+        message = user.text
+    else:
+        message = "unknow action yet"
+
+    return message
